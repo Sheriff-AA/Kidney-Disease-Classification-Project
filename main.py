ba212934b09ad55306275cec2e912c3dcb3bd0eb
Sherif Atitebi <sherifatitebi@gmail.com>
2023-12-26 19:02:27 +0000

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from src.kidneyDiseaseClassifier import logger
+from kidneyDiseaseClassifier import logger
+from kidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("Welcome to our custom log")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
