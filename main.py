4e84eaba13b2ee72298871fbcf40efbf59f663b0
Sherif Atitebi <sherifatitebi@gmail.com>
2024-01-12 23:23:29 +0000

    model evaluation and mlflow implementation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kidneyDiseaseClassifier import logger
 from kidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from kidneyDiseaseClassifier.pipeline.stage_02_prepare_base_model import PreparedBaseModelTrainingPipeline
 from kidneyDiseaseClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from kidneyDiseaseClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -34,6 +35,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation Stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/kidneyDiseaseClassifier/config/configuration.py
+++ src/kidneyDiseaseClassifier/config/configuration.py
@@ -1,9 +1,10 @@
 from kidneyDiseaseClassifier.constants import *
 import os
-from kidneyDiseaseClassifier.utils.common import read_yaml, create_directories
+from kidneyDiseaseClassifier.utils.common import read_yaml, create_directories, save_json
 from kidneyDiseaseClassifier.entity.config_entity import (DataIngestionConfig,
                                                           PrepareBaseModelConfig,
-                                                          TrainingConfig)
+                                                          TrainingConfig,
+                                                          EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -83,3 +84,15 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Kidney-Tumor-Normal Classification Images",
+            mlflow_uri="https://dagshub.com/sherifatitebi/Kidney-Disease-Classification-Project.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
